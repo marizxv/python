fb7877cc326ebbb4afc3b1b4dd3f7029006fe1c0
maria <marizxv@gmail.com>
2025-01-21 23:53:22 +0100

    cleaned output in students_sr.py; added new vehicles.py project with related files

--- students_sr.py
+++ students_sr.py
@@ -18,40 +18,40 @@ class Uczelnia1:
     rodzaj_uczelni = None
     glowny_kierunek = None
     przedmioty = []
-    odna_nazva = None
-    def __init__(self, rodzaj_uczelni, glowny_kierunek, przedmioty, odna_nazva):
+    cos_unikatowe = None
+    def __init__(self, rodzaj_uczelni, glowny_kierunek, przedmioty, cos_unikatowe):
         Uczelnia1.rodzaj_uczelni = rodzaj_uczelni
         Uczelnia1.glowny_kierunek = glowny_kierunek
         Uczelnia1.przedmioty = przedmioty
-        Uczelnia1.odna_nazva = odna_nazva
+        Uczelnia1.cos_unikatowe = cos_unikatowe
 
 class Uczelnia2:
     nazwa = "Politechnika Łódzka"
     rodzaj_uczelni = None
     glowny_kierunek = None
     przedmioty = []
-    insha_nazva = None
-    def __init__ (self, rodzaj_uczelni, glowny_kierunek, przedmioty, insha_nazva):
+    inne_unikatowe = None
+    def __init__ (self, rodzaj_uczelni, glowny_kierunek, przedmioty, inne_unikatowe):
         Uczelnia2.rodzaj_uczelni = rodzaj_uczelni
         Uczelnia2.glowny_kierunek = glowny_kierunek
         Uczelnia2.przedmioty = przedmioty
-        Uczelnia2.insha_nazva = insha_nazva
+        Uczelnia2.inne_unikatowe = inne_unikatowe
 
 class Student(Czlowiek, Uczelnia1, Uczelnia2):
     postep = {}
     def __init__(self, imie, nazwisko, data_urodzenia, plec, pesel, uczelnia, postep = None):
-        # Inicjalizacja klasy Czlowiek
+
         Czlowiek.__init__(self, imie, nazwisko, data_urodzenia, plec, pesel)
 
         if len(uczelnia) == 2:
-            Uczelnia1.__init__(self, uczelnia[0][1], uczelnia[0][2], uczelnia[0][3], 'test1')
-            Uczelnia2.__init__(self, uczelnia[1][1], uczelnia[1][2], uczelnia[1][3], 'test2')
+            Uczelnia1.__init__(self, uczelnia[0][1], uczelnia[0][2], uczelnia[0][3], 'Mam wieczory pizzy!')
+            Uczelnia2.__init__(self, uczelnia[1][1], uczelnia[1][2], uczelnia[1][3], 'A ja nie mam, ale nie szkodzi!')
         elif uczelnia[0] == Uczelnia1.nazwa:
-            Uczelnia1.__init__(self, uczelnia[1], uczelnia[2], uczelnia[3], 'test1')
+            Uczelnia1.__init__(self, uczelnia[1], uczelnia[2], uczelnia[3], 'Mam wieczory pizzy!')
             Uczelnia2.__init__(self, None, None, [], None)
         elif uczelnia[0] == Uczelnia2.nazwa:
             Uczelnia1.__init__(self, None, None, [], None)
-            Uczelnia2.__init__(self, uczelnia[1], uczelnia[2], uczelnia[3], 'test2')
+            Uczelnia2.__init__(self, uczelnia[1], uczelnia[2], uczelnia[3], 'A ja nie mam, ale nie szkodzi!')
 
         if len(Uczelnia1.przedmioty) > 0 and len(Uczelnia2.przedmioty) > 0:
             Student.set_postep(self, Uczelnia1, postep[0])
@@ -88,8 +88,16 @@ for student_info in students_data:
         student_info["data_urodzenia"],
         student_info["plec"],
         student_info["pesel"],
-        [student_info["university_name"], student_info["university_type"], student_info["university_glowny_kierunek"], student_info["university_subjects"]],
+        [student_info["university_name"], student_info["university_type"],
+                 student_info["university_glowny_kierunek"], student_info["university_subjects"]],
         student_info["progress"]
     )
-    print(student.imie, student.nazwisko, student.get_postep())
+    print(f"{student.imie}, {student.nazwisko}, {student.data_urodzenia}")
+    for uni, przedmioty in student.get_postep().items():
+        print(f"Uczelnia: {uni}")
+        for przedmiot_ocena in przedmioty:
+            for przedmiot, ocena in przedmiot_ocena.items():
+                print(f" - {przedmiot}: {ocena}%")
+    print("=" * 50)
+
 
